915173fb04ce459a72b66314d2b150d4b6ee97b3
iyappan10 <52095660+iyappan10@users.noreply.github.com>
2019-08-04 10:45:56 +0530

    Update and rename README.md to Hs1f1.py

---
+++ Hs1f1.py
@@ -0,0 +1,13 @@
+def Repeat(x): 
+    size = len(x) 
+    repeated = [] 
+    for i in range(size): 
+        k = i + 1
+        for j in range(k, size): 
+            if x[i] == x[j] and x[i] not in repeated: 
+                repeated.append(x[i]) 
+    return repeated   
+Num=int(input())
+list1=list(map(int,input().split()))
+n=sorted(Repeat(list1))
+print (*n)
