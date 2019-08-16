617265b0d21d603de44f1fefba8da775b25a585f
iyappan10 <52095660+iyappan10@users.noreply.github.com>
2019-08-16 11:17:22 +0530

    Create Hs10f1.py

---
+++ Hs10f1.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+num=list(map(int,input().split()))
+mum=list(map(int,input().split()))
+sum=0
+for i in range(len(num)):
+    for j in range(len(mum)):
+         if(mum[j]==num[i]):
+             sum=sum+1
+if(sum==n-1):
+    print('yes')
+else:
+    print('no')
