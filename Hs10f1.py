6de50a1a2f742b52d14189f05e24540b6e182566
iyappan10 <52095660+iyappan10@users.noreply.github.com>
2019-08-16 11:25:16 +0530

    Update Hs10f1.py

--- Hs10f1.py
+++ Hs10f1.py
@@ -6,7 +6,9 @@ for i in range(len(num)):
     for j in range(len(mum)):
          if(mum[j]==num[i]):
              sum=sum+1
-if(sum!=0):
+if(sum<n):
     print('YES')
+elif(sum==n):
+    print('NO')
 else:
     print('NO')
